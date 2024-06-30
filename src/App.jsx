a4739ee144768f5ac73ce996ed01f6eadb9c0e22
Srujan S P <srujansjapthi@gmail.com>
2024-06-30 10:59:00 +0530

    Update 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import './App.css'
 import LandingPage from './pages/LandingPage'
 import NavBar from './components/NavBar'
-import AboutUs from './components/AboutUs'
 import LoginPage from './pages/LoginPage'
 import SignUpPage from './pages/SignUpPage'
 
 
 function App() {
 	return (
-	//<LoginPage></LoginPage>
-	<SignUpPage></SignUpPage>
+	// <LoginPage></LoginPage>
+	// <SignUpPage></SignUpPage>
+	<LandingPage />
 	// <Route>
 	// 	<Route path="/" element={<LoginPage/>}/>
 	// </Route>
